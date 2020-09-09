3d36601998817af0d4b2b8a031480f7b4b9efb54
Jeremy Felt <jeremy.felt@gmail.com>
2020-09-09 10:49:44 -0700

    Adjust plugin author display

--- plugin.php
+++ plugin.php
@@ -3,7 +3,8 @@
 Plugin Name: Organizational
 Plugin URI: https://github.com/happyprime/organizational
 Description: Associate people, projects, organizations, and publications.
-Author: happyprime
+Author: Happy Prime
+Author URI: https://happyprime.co
 Version: 1.0.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
