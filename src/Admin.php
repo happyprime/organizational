8d1c7b6a2b77f9ad52f5696ea4ab18acb8664b93
jeremyfelt <jeremy.felt@gmail.com>
2024-10-30 14:48:13 -0700

    Improve checks for post types in admin interface

--- src/Admin.php
+++ src/Admin.php
@@ -51,9 +51,27 @@ class Admin {
 	public static function sanitize_names( $names ): array {
 		global $organizational;
 
+		$supported_post_types = array();
+
+		if ( isset( $organizational->projects ) ) {
+			$supported_post_types[] = $organizational->projects->post_type;
+		}
+
+		if ( isset( $organizational->people ) ) {
+			$supported_post_types[] = $organizational->people->post_type;
+		}
+
+		if ( isset( $organizational->entities ) ) {
+			$supported_post_types[] = $organizational->entities->post_type;
+		}
+
+		if ( isset( $organizational->publications ) ) {
+			$supported_post_types[] = $organizational->publications->post_type;
+		}
+
 		$clean_names = array();
 		foreach ( $names as $name => $data ) {
-			if ( ! in_array( $name, array( $organizational->projects->post_type, $organizational->people->post_type, $organizational->entities->post_type, $organizational->publications->post_type ), true ) ) {
+			if ( ! in_array( $name, $supported_post_types, true ) ) {
 				continue;
 			}
 
@@ -96,54 +114,74 @@ class Admin {
 	public static function general_settings_names(): void {
 		global $organizational;
 
+		if ( ! isset( $organizational ) || ! is_object( $organizational ) ) {
+			return;
+		}
+
 		$names = get_option( 'organizational_names', false );
 
 		$display_names = array();
-		if ( ! isset( $names[ $organizational->projects->post_type ] ) ) {
+
+		if ( isset( $organizational->projects ) && ! isset( $names[ $organizational->projects->post_type ] ) ) {
 			$names[ $organizational->projects->post_type ] = array();
 		}
-		if ( ! isset( $names[ $organizational->people->post_type ] ) ) {
+
+		if ( isset( $organizational->people ) && ! isset( $names[ $organizational->people->post_type ] ) ) {
 			$names[ $organizational->people->post_type ] = array();
 		}
-		if ( ! isset( $names[ $organizational->entities->post_type ] ) ) {
+
+		if ( isset( $organizational->entities ) && ! isset( $names[ $organizational->entities->post_type ] ) ) {
 			$names[ $organizational->entities->post_type ] = array();
 		}
-		if ( ! isset( $names[ $organizational->publications->post_type ] ) ) {
+
+		if ( isset( $organizational->publications ) && ! isset( $names[ $organizational->publications->post_type ] ) ) {
 			$names[ $organizational->publications->post_type ] = array();
 		}
 
-		$display_names[ $organizational->projects->post_type ]     = wp_parse_args(
-			$names[ $organizational->projects->post_type ],
-			array(
-				'singular' => $organizational->projects->singular_name,
-				'plural'   => $organizational->projects->plural_name,
-			)
-		);
-		$display_names[ $organizational->people->post_type ]       = wp_parse_args(
-			$names[ $organizational->people->post_type ],
-			array(
-				'singular' => $organizational->people->singular_name,
-				'plural'   => $organizational->people->plural_name,
-			)
-		);
-		$display_names[ $organizational->entities->post_type ]     = wp_parse_args(
-			$names[ $organizational->entities->post_type ],
-			array(
-				'singular' => $organizational->entities->singular_name,
-				'plural'   => $organizational->entities->plural_name,
-			)
-		);
-		$display_names[ $organizational->publications->post_type ] = wp_parse_args(
-			$names[ $organizational->publications->post_type ],
-			array(
-				'singular' => $organizational->publications->singular_name,
-				'plural'   => $organizational->publications->plural_name,
-			)
-		);
+		if ( isset( $organizational->projects ) ) {
+			$display_names[ $organizational->projects->post_type ] = wp_parse_args(
+				$names[ $organizational->projects->post_type ],
+				array(
+					'singular' => $organizational->projects->singular_name,
+					'plural'   => $organizational->projects->plural_name,
+				)
+			);
+		}
+
+		if ( isset( $organizational->people ) ) {
+			$display_names[ $organizational->people->post_type ] = wp_parse_args(
+				$names[ $organizational->people->post_type ],
+				array(
+					'singular' => $organizational->people->singular_name,
+					'plural'   => $organizational->people->plural_name,
+				)
+			);
+		}
+
+		if ( isset( $organizational->entities ) ) {
+			$display_names[ $organizational->entities->post_type ] = wp_parse_args(
+				$names[ $organizational->entities->post_type ],
+				array(
+					'singular' => $organizational->entities->singular_name,
+					'plural'   => $organizational->entities->plural_name,
+				)
+			);
+		}
+
+		if ( isset( $organizational->publications ) ) {
+			$display_names[ $organizational->publications->post_type ] = wp_parse_args(
+				$names[ $organizational->publications->post_type ],
+				array(
+					'singular' => $organizational->publications->singular_name,
+					'plural'   => $organizational->publications->plural_name,
+				)
+			);
+		}
 		?>
 		<div class="organizational-settings-names">
 			<p>Changing the settings here will override the default labels for the content types provided by the Organizational plugin. The default labels are listed to the left of each field. The <strong>singular</strong> label will also be used as a slug in URLs.</p>
 
+			<?php if ( isset( $organizational->projects ) ) : ?>
 			<div>
 				<label for="<?php echo esc_attr( self::get_id( $organizational->projects->post_type, 'singular' ) ); ?>">Project (Singular)</label>
 				<input
@@ -165,7 +203,9 @@ class Admin {
 					class="regular-text"
 				/>
 			</div>
+			<?php endif; ?>
 
+			<?php if ( isset( $organizational->people ) ) : ?>
 			<div>
 				<label for="organizational_names_people_singular">Person (Singular)</label>
 				<input
@@ -187,7 +227,9 @@ class Admin {
 					class="regular-text"
 				/>
 			</div>
+			<?php endif; ?>
 
+			<?php if ( isset( $organizational->entities ) ) : ?>
 			<div>
 				<label for="organizational_names_entity_singular">Entity (Singular)</label>
 				<input
@@ -209,7 +251,9 @@ class Admin {
 					class="regular-text"
 				/>
 			</div>
+			<?php endif; ?>
 
+			<?php if ( isset( $organizational->publications ) ) : ?>
 			<div>
 				<label for="organizational_names_publication_singular">Publication (Singular)</label>
 				<input
@@ -231,6 +275,7 @@ class Admin {
 					class="regular-text"
 				/>
 			</div>
+			<?php endif; ?>
 		</div>
 		<?php
 	}
